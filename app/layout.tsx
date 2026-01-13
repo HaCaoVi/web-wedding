d522bc1ee638a62ba4f287f30b3cdcf5e70e2543
richieLio <lhuyhoang18903@gmail.com>
2026-01-13 18:12:53 +0700

    UPDATE:: font

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,36 @@ import type { Metadata } from "next"
 import { Playfair_Display, Lora } from "next/font/google"
 import "./globals.css"
 import { Analytics } from "@vercel/analytics/next"
+import localFont from "next/font/local"
+
+const dancingScript = localFont({
+  src: "../public/font/Dancing_Script/DancingScript-VariableFont_wght.ttf",
+  variable: "--font-dancing",
+  display: "swap",
+})
+
+const gaegu = localFont({
+  src: [
+    {
+      path: "../public/font/Gaegu/Gaegu-Light.ttf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../public/font/Gaegu/Gaegu-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../public/font/Gaegu/Gaegu-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+  variable: "--font-gaegu",
+  display: "swap",
+})
+
 
 const _playfairDisplay = Playfair_Display({ subsets: ["latin"], variable: "--font-serif" })
 const _lora = Lora({ subsets: ["latin"], variable: "--font-serif-body" })
@@ -30,7 +60,13 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head></head>
-      <body className={`${_playfairDisplay.variable} ${_lora.variable} font-sans antialiased overflow-x-hidden`}>
+      <body
+        className={`
+          ${_playfairDisplay.variable}
+          ${_lora.variable}
+          ${dancingScript.variable}
+          ${gaegu.variable}
+          font-sans antialiased overflow-x-hidden`}>
         {children}
         <Analytics />
       </body>
